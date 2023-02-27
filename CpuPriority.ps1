507f1d38572fccca6c30b02cb4308540d5a8dc6c
garretsidzaka <ringworldwu@gmail.com>
2023-02-27 10:55:09 -0700

    Create CpuPriority.ps1
    initial commit

---
+++ CpuPriority.ps1
@@ -0,0 +1,11 @@
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+
+
+while ($true)
+{
+	sleep 10
+	Get-WmiObject Win32_process -filter 'name = "Torch.Server.exe"' | foreach-object { $_.SetPriority(32768) }
+	sleep 600
+}
